ed509467e79b71ad25ee1d6ef02d0d7c9cb47a14
eunyoung <whdmsdud77@naver.com>
2023-03-03 11:37:48 +0900

    eunoyung

---
+++ frontend/src/views/user/uMainView.vue
@@ -0,0 +1,141 @@
+<template>
+
+  <div>
+    <div>
+      <h2>최근 인기 있는 공고</h2>
+      <div>
+        <div>
+          <img src="#">
+          <h3>(주)카카오</h3>
+          <h5>2023년도 각 부문별 채용모집</h5>
+        </div>
+
+        <div>
+          <img src="#">
+          <h3>(주)카카오</h3>
+          <h5>2023년도 각 부문별 채용모집</h5>
+        </div>
+
+        <div>
+          <img src="#">
+          <h3>(주)카카오</h3>
+          <h5>2023년도 각 부문별 채용모집</h5>
+        </div>
+
+        <div>
+          <img src="#">
+          <h3>(주)카카오</h3>
+          <h5>2023년도 각 부문별 채용모집</h5>
+        </div>
+
+        <div>
+          <img src="#">
+          <h3>(주)카카오</h3>
+          <h5>2023년도 각 부문별 채용모집</h5>
+        </div>
+
+        <div>
+          <img src="#">
+          <h3>(주)카카오</h3>
+          <h5>2023년도 각 부문별 채용모집</h5>
+        </div>
+      </div>
+
+    </div>
+
+
+    <div>
+      <h3>Q&A</h3>
+      <table>
+        <thead>
+        <tr>
+          <td>작성자</td>
+          <td>질문제목</td>
+          <td>등록시간</td>
+        </tr>
+        </thead>
+
+        <tbody>
+        <tr>
+          <td>Id</td>
+          <td>Title</td>
+          <td>Regdate</td>
+        </tr>
+        <tr>
+          <td>Id</td>
+          <td>Title</td>
+          <td>Regdate</td>
+        </tr>
+        <tr>
+          <td>Id</td>
+          <td>Title</td>
+          <td>Regdate</td>
+        </tr>
+        <tr>
+          <td>Id</td>
+          <td>Title</td>
+          <td>Regdate</td>
+        </tr>
+        <tr>
+          <td>Id</td>
+          <td>Title</td>
+          <td>Regdate</td>
+        </tr>
+
+
+
+        </tbody>
+
+
+      </table>
+
+
+    </div>
+
+    <!--우측 회원정보 확인하는 곳-->
+    <div>
+
+      <div>
+        <img src="#">
+        <h3>OOO님의 입사 지원 현황</h3>
+        <a href="#">쪽지(3)</a>
+      </div>
+
+      <div>
+        <ul>
+          <li>10</li>
+          <li>지원완료</li>
+        </ul>
+
+        <ul>
+          <li>10</li>
+          <li>열람</li>
+        </ul>
+
+        <ul>
+          <li>10</li>
+          <li>미열람</li>
+        </ul>
+
+      </div>
+
+      <div>
+        <button>이력서 관리</button>
+        <button>mypage</button>
+      </div>
+
+    </div>
+
+  </div>
+
+</template>
+
+<script>
+export default {
+  name: "uMainView"
+}
+</script>
+
+<style scoped>
+
+</style>
