c140fb8fb0964c3394b876a635332cf7a95a00b0
박희진 <heejin@baghuijin-ui-MacBookAir.local>
2023-03-06 11:01:14 +0900

    20230306 박희진 css작업

--- frontend/src/router/index.js
+++ frontend/src/router/index.js
@@ -14,6 +14,16 @@ const routes = [
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
     component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+  },
+  {
+    path: '/uJoinView',
+    name: 'uJoinView',
+    component: () => import('../views/user/uJoinView.vue')
+  },
+  {
+    path: '/cMainView',
+    name: 'cMainView',
+    component: () => import('../views/company/cMainView.vue')
   }
 ]
 
