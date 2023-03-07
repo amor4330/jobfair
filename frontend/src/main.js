5dfef4afcca462afaa66a01a7b95392518cacda3
sschoi1994 <sschoi1994@naver.com>
2023-03-07 11:00:47 +0900

    login & jwt~ing & bootstrap

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -3,8 +3,12 @@ import App from './App.vue'
 import router from './router'
 import store from './store'
 import axios from 'axios'
+import BootstrapVue3 from 'bootstrap-vue-3'
+import 'bootstrap/dist/css/bootstrap.css'
+import 'bootstrap-vue-3/dist/bootstrap-vue-3.css'
 
 const app = createApp(App)
+app.use(BootstrapVue3)
 app.config.globalProperties.$axios = axios
 app.use(router)
 app.use(store)
